ba3ec5832c7c3e66fbe2f715f0827963e1f1ce78
morimoto <morimoto@cygnus-internet.co.jp>
2017-07-25 15:14:10 +0900

    削除

--- petland/js/map_click.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('.map_click iframe').css('pointer-events', 'none');
-
-    $(".map_click").click(function(){
-        $('.map_click iframe').css('pointer-events', 'auto');
-    });
-});
